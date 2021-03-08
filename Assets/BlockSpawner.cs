45e2b43bb018eab763cae211cffbfbe35ff903a6
febert82 <febert82@gmail.com>
2021-03-08 18:21:12 +0100

    Initial commit

---
+++ Assets/BlockSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject spawnPrefab;
+
+    public float timeBetweenWaves = 1f;
+
+    private float timeToSpawn = 2f;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Time.time >= timeToSpawn)
+        {
+            SpawnBlocks();
+            timeToSpawn = Time.time + timeBetweenWaves;
+        }
+    }
+
+    void SpawnBlocks()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(spawnPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
